b80a89148367c865d451417235c43b503f16223f
Shalu Dwivedi <s.shalu.dwivedi@gmail.com>
2020-09-09 10:27:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+### library required for dashboard
+
+import os
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+### defining the HTML component
+
+app.layout = html.Div([html.Div("Welcome to the dashboard", style= { "color": "white", "text-align": "center", "background-color": "blue",   "display":"block", 'font-size':'5vw', 'marginLeft': 100, 'marginRight': 100, 'marginTop': 30, 'marginBottom': 100, 'border': 'thin lightgrey dashed', 'padding':'10px 10px 10px 10px'}),
+                       html.Div("Here is my first dashboard", style= { "color": "green", "text-align": "center", "background-color": "primary", "display":"block", 'font-size':'5vw', 'marginLeft': 100, 'marginRight':100, 'marginTop': 30, 'marginBottom': 100, 'border': 'thin lightgrey dashed', 'padding': '10px 10px 10px 10px'})])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
